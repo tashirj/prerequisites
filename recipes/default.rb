5444ef5bbff54470340415e6f3e42c58e4789742
tushar <tushdudhade@gmail.com>
2016-06-22 14:42:52 +0530

    Missing package of build-essential added

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 #package ["libreadline-dev","libncurses5-dev", "libpcre3-dev", "libssl-dev", "perl", "make"] do
 #  action :install
 #end
-%w{"libreadline-dev","libncurses5-dev", "libpcre3-dev", "libssl-dev", "perl", "make"}.each do |pkg|
+%w{"libreadline-dev","libncurses5-dev", "libpcre3-dev", "libssl-dev", "perl", "make", "build-essential"}.each do |pkg|
    package pkg do
       action :install
    end
