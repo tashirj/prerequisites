ab9731b147080efbd87721a5ab57a44de2a81a56
tushar <tushdudhade@gmail.com>
2016-06-22 14:19:42 +0530

    Package installation method changed

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,8 @@
-package ["libreadline-dev","libncurses5-dev", "libpcre3-dev", "libssl-dev", "perl", "make"] do
-  action :install
+#package ["libreadline-dev","libncurses5-dev", "libpcre3-dev", "libssl-dev", "perl", "make"] do
+#  action :install
+#end
+%w{"libreadline-dev","libncurses5-dev", "libpcre3-dev", "libssl-dev", "perl", "make"}.each do |pkg|
+   package pkg do
+      action :install
+   end
 end
